37fc7a277e1d34f427d0b5e4e26f1762fa34d049
Shiva Krishna Merla <shivakrishna.merla@hpe.com>
2019-11-04 14:50:35 -0800

    Validate if staging device info is current and appropriate mount/device exists on host. (#66)
    * Validate if staging device info is current and appropriate mount/device exists on host.
    
    Signed-off-by: Shiva Krishna, Merla <shivakrishna.merla@hpe.com>
    
    * Fix comments
    
    Signed-off-by: Shiva Krishna, Merla <shivakrishna.merla@hpe.com>

--- pkg/driver/node_server.go
+++ pkg/driver/node_server.go
@@ -293,19 +293,34 @@ func (driver *Driver) isVolumeStaged(
 		return false, nil // Not staged as volume id mismatch
 	}
 
-	// Check if the staged device exists. If error (i.e, device is missing), then return as 'Not staged'
-	mounts, err := driver.chapiDriver.GetMountsForDevice(stagingDev.Device)
-	if err != nil {
-		log.Infof("Device %+v not present on the host", stagingDev.Device)
-		return false, nil // Not staged as device doesn't exist on the host
-	}
-	log.Tracef("Found %v mounts for device with serial number %v", len(mounts), stagingDev.Device.SerialNumber)
+	if volAccessType == model.MountType {
+		// Check if the staged device exists. If error (i.e, device is missing), then return as 'Not staged'
+		mounts, err := driver.chapiDriver.GetMountsForDevice(stagingDev.Device)
+		if err != nil || len(mounts) == 0 {
+			log.Infof("Device %+v not present on the host", stagingDev.Device)
+			return false, nil // Not staged as device doesn't exist on the host
+		}
+		log.Tracef("Found %v mounts for device with serial number %v", len(mounts), stagingDev.Device.SerialNumber)
 
-	// For each mount, check if the device is active. If inactive, then return as "Unstaged"
-	for _, mount := range mounts {
-		if mount.Device != nil && mount.Device.State != model.ActiveState.String() {
-			log.Tracef("Found device state as '%s' for mountpoint %s. So, returning as unstaged", mount.Device.State, mount.Mountpoint)
-			return false, nil // Not staged as device is inactive
+		foundMount := false
+		for _, mount := range mounts {
+			if stagingDev.MountInfo != nil && mount.Mountpoint == stagingDev.MountInfo.MountPoint {
+				foundMount = true
+				break
+			}
+		}
+		if !foundMount {
+			log.Infof("Device %+v not mounted on the host", stagingDev.Device)
+			return false, nil
+		}
+	}
+
+	if volAccessType == model.BlockType {
+		// Check if path exists
+		exists, _, _ := util.FileExists(stagingDev.Device.AltFullPathName)
+		if !exists {
+			log.Infof("Device path %s does not exist on the node", stagingDev.Device.AltFullPathName)
+			return false, nil // Not staged yet as targetPath does not exists
 		}
 	}
 
