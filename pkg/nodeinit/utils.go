858f07481c256d6cb71af56989c47c423a89435e
AnushaY1916 <anuanusha471@gmail.com>
2024-05-10 13:53:18 +0530

    Minor improvements
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>

--- pkg/nodeinit/utils.go
+++ pkg/nodeinit/utils.go
@@ -8,31 +8,6 @@ import (
 	storage_v1 "k8s.io/api/storage/v1"
 )
 
-func GetMultipathDevices(nodeName string) (multipathDevices []*model.MultipathDeviceInfo, err error) {
-	multipathDevices, err = tunelinux.GetMultipathDevices() //driver.GetMultipathDevices()
-	if err != nil {
-		log.Errorf("Error while getting the multipath devices on the node %s", nodeName)
-		// This line will be uncommented once the full imeplementation of this stale entry fix is done
-		//return err
-		return nil, err //Remove later
-	}
-	if multipathDevices != nil && len(multipathDevices) > 0 {
-		for _, device := range multipathDevices {
-			//TODO: Assess whether the device belongs to this node or not and whether to do clean up or not
-			log.Tracef("Name:%s Vendor:%s Paths:%f Path Faults:%f UUID:%s IsUnhealthy:%t", device.Name, device.Vendor, device.Paths, device.PathFaults, device.UUID, device.IsUnhealthy)
-			//Remove Later
-			if device.IsUnhealthy {
-				log.Infof("Multipath device %s on the node %s is unhealthy", nodeName, device.Name)
-			} else {
-				log.Infof("Multipath device %s on the node %s is healthy", nodeName, device.Name)
-			}
-		}
-	} else {
-		log.Tracef("No multipath devices found on the node %s", nodeName)
-	}
-	return multipathDevices, nil
-}
-
 func doesDeviceBelongToTheNode(multipathDevice *model.MultipathDeviceInfo, volumeAttachmentList *storage_v1.VolumeAttachmentList, nodeName string) bool {
 	if multipathDevice != nil {
 		for _, va := range volumeAttachmentList.Items {
@@ -49,6 +24,8 @@ func doesDeviceBelongToTheNode(multipathDevice *model.MultipathDeviceInfo, volum
 }
 
 func AnalyzeMultiPathDevices(flavor flavor.Flavor, nodeName string) error {
+	log.Trace(">>>>> AnalyzeMultiPathDevices")
+	defer log.Trace("<<<<< AnalyzeMultiPathDevices")
 
 	multipathDevices, err := tunelinux.GetMultipathDevices() //driver.GetMultipathDevices()
 	if err != nil {
@@ -103,6 +80,6 @@ func AnalyzeMultiPathDevices(flavor flavor.Flavor, nodeName string) error {
 				//Nothing to do
 			}
 		}
-	}
+	} // end-for loop
 	return nil
 }
