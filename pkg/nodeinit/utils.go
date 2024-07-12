bc5f82b59605930a26a5b9481937f11c3a6be31d
AnushaY1916 <anuanusha471@gmail.com>
2024-07-12 07:32:41 +0530

    Added full path for fuser command
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>

--- pkg/nodeinit/utils.go
+++ pkg/nodeinit/utils.go
@@ -145,7 +145,7 @@ func cleanup(device *model.MultipathDevice) error {
 	log.Tracef(">>>>> Cleaning up the multipath device %s", device.Name)
 	defer log.Trace("<<<<< cleanup")
 
-	err := tunelinux.KillProcessesUsingMountPoints(device.Name)
+	err := tunelinux.KillProcessesUsingMountPoints("/dev/mapper/" + device.Name)
 	if err != nil {
 		log.Errorf("Unable to kill the processes using the multipath device's references %s: %s", device.Name, err.Error())
 		return err
