ab2b4bffe0d253ac134881b040aeadc2e560c4ca
Anusha Y <anuanusha471@gmail.com>
2024-12-10 13:14:02 +0530

    CON-2549: NFS Volume Expansion Part-2 (#423)
    * Update to handle Nimble volumes
    
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>
    
    * Update to handle Nimble volumes
    
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>
    
    * Made improvements
    
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>
    
    * Made improvements for accessmode checks
    
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>
    
    * refined some logger messages
    
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>
    
    * Added comments
    
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>
    
    ---------
    
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>

--- pkg/flavor/vanilla/flavor.go
+++ pkg/flavor/vanilla/flavor.go
@@ -122,10 +122,6 @@ func (flavor *Flavor) GetNFSVolumeID(volumeID string) (string, error) {
 	return "", nil
 }
 
-func (flavor *Flavor) IsNFSVolumeExpandable(volumeId string) bool {
-	return false
-}
-
 func (flavor *Flavor) ExpandNFSBackendVolume(nfsVolumeID string, newCapacity int64) error {
 	return nil
 }
