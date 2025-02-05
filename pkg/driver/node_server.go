4fa30e45ffd1aafe31c0537a60c2d0750b8917ac
Shrey Deshmukh <shrey.deshmukh@hpe.com>
2025-02-05 10:34:46 +0000

    CON-3170:Fix the failures seen in CSI Sanity unit-test case execution
    Signed-off-by: Shrey Deshmukh <shrey.deshmukh@hpe.com>

--- pkg/driver/node_server.go
+++ pkg/driver/node_server.go
@@ -2062,9 +2062,6 @@ func (driver *Driver) NodeGetInfo(ctx context.Context, req *csi.NodeGetInfoReque
 	}
 
 	topo := &csi.Topology{Segments: accessableTopologySegments}
-	// if len(accessableTopologySegments) == 0 {
-	// 	topo = nil
-	// }
 
 	log.Infof("node %s topo: %+v", nodeInfo.Name, topo)
 	// Get max volume per node from environment variable
